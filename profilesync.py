e5f874980ffa0cde3292c3d603367ae5d7d66407
Hylence <bose3110himank@gmail.com>
2023-04-22 00:29:34 +0530

    Almost completed creating profile code- 1. Added check for creating invalid file names. 2. Added code for saving of important values inside the preset.ini. 3. Tested on cmd.

--- profilesync.py
+++ profilesync.py
@@ -4,6 +4,7 @@ from colorama import init, Fore, Style
 from file_folder_manager import FileFolderManager
 from menu import Menu
 import common_utilities as cu
+from input_output_file_operations import InputOutputFileOperations
 
 def listProfiles(parent_folder, profile_name):
     profile_folders = []
@@ -50,16 +51,45 @@ def main(pm, cu):
             profileName = ""
             while not profileName.strip():
                 cu.clear_screen()
+                invalid_names = ['CON', 'PRN', 'AUX', 'NUL']
+                invalid_names += ['COM{}'.format(i) for i in range(1, 10)]
+                invalid_names += ['LPT{}'.format(i) for i in range(1, 10)]
+                
                 profileName = input("Profile Name: ")
-            profilePath = pm.join_custom_path(sync_path, profileName + ".ini")
-            if ffm.is_file_present(profilePath):
-                cu.clear_screen()
-                print("Profile with this name already exists, please try other names...")
-                print("Returning back...")
-                time.sleep(1)
-            else:
-                if not ffm.is_file_present(profilePath):
-                    ffm.create_file(profilePath)
-                cu.clear_screen()
-                print("Successfully Created Profile")
-                time.sleep(1)
+                
+                if not any(char in profileName for char in ['<', '>', ':', '"', '/', '\\', '|', '?', '*']) and not profileName.upper() in invalid_names:
+                
+                    profilePath = pm.join_custom_path(sync_path, profileName + ".ini")
+                    if ffm.is_file_present(profilePath):
+                        cu.clear_screen()
+                        print("Profile with this name already exists, please try other names...")
+                        print("Returning back...")
+                        time.sleep(1)
+                    else:
+                        if not ffm.is_file_present(profilePath):
+                            ffm.create_file(profilePath)
+                        cu.clear_screen()
+                        print("Successfully Created Profile")
+                        time.sleep(1)
+                        cu.clear_screen()
+                        source = InputOutputFileOperations(cfg_path=profilePath, key='source', prompt_message='Enter your source path-> ', delimiter='->')
+                        source.input_from_user()
+                        source.write_to_file()
+                        time.sleep(1)
+                        destn = InputOutputFileOperations(cfg_path=profilePath, key='destination', prompt_message='\n\nEnter your destination path->', delimiter='-> ')
+                        destn.input_from_user()
+                        destn.write_to_file()
+                        time.sleep(1)
+                        inct = InputOutputFileOperations(cfg_path=profilePath, key='interactive', prompt_message='\n\nDo you want interactive method (Y/N)= ')
+                        inct.input_from_user()
+                        inct.write_to_file()
+                        time.sleep(1)
+                        dry_run = InputOutputFileOperations(cfg_path=profilePath, key='dry_run', prompt_message='\n\nDo you want to dry run before proceeding (Y/N)= ')
+                        dry_run.input_from_user()
+                        dry_run.write_to_file()
+                        time.sleep(1)
+                else:
+                    cu.clear_screen()
+                    print("Invalid profile name..")
+                    print("Try Again..")
+                    time.sleep(1)
